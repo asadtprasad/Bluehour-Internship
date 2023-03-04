a7845c6567f7753baf577931e8411f0b44db14d3
Azadhi <asad.mca2123@saintgits.org>
2023-03-04 20:57:45 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Contact  from './Contact';
+import Welcome from './Welcome';
+
+
+
+
+function App() {
+  
+  return (
+   <div>
+    <Welcome title="hai how are you"/>
+     <Welcome title="hai da 
+     "/>
+   </div>
+     
+ 
+  );
+}
+
+export default App;
+
